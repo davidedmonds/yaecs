69aef3252f605756e589b4b7c1e4b661f993c8b0
David Edmonds <edmonds.d.r@gmail.com>
2016-11-24 17:13:29 +0000

    Add back entity processing using a specialization of system.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,15 +1,21 @@
 extern crate yaecs;
 
-use yaecs::system::System;
+use std::any::TypeId;
+
+use yaecs::entity::Entity;
+use yaecs::global::Globals;
+use yaecs::system::{EntitySystem, System};
 use yaecs::world::{World, WorldData};
 
 fn main() {
   let mut world = World::new();
   world.register(Box::new(BasicSystem::new()));
+  world.register(Box::new(BasicEntitySystem {}));
   world.create_entity(| components | {
     components.insert(BasicComponent(128));
     components.insert(OtherBasicComponent {});
   });
+  world.create_entity(| _ | {});
   world.update();
 }
 
@@ -28,5 +34,17 @@ impl BasicSystem {
 impl System for BasicSystem {
   fn process(&self, world_data: &mut WorldData) {
     println!("Processing world_data {:?}", world_data);
+  }
+}
+
+struct BasicEntitySystem;
+
+impl EntitySystem for BasicEntitySystem {
+  fn operates_on(&self) -> Vec<TypeId> {
+    vec!(TypeId::of::<BasicComponent>())
+  }
+
+  fn process_entities(&self, entities: Vec<&Entity>, globals: &Globals) {
+    println!("Processing entities {:?} with globals {:?}", entities, globals);
   }
 }
--- src/system.rs
+++ src/system.rs
@@ -1,8 +1,30 @@
 //! `System`s operate directly on a `World`.
+use entity::Entity;
+use global::Globals;
 use world::WorldData;
+
+use std::any::TypeId;
 
 /// A `System` operates directly on a `World`.
 pub trait System {
   /// Operate on the `World` according to the purpose of this `System`.
   fn process(&self, world_data: &mut WorldData);
+}
+
+/// A `System` specialised in working with `Entity`s filtered by component.
+pub trait EntitySystem {
+  /// `TypeId`s of the components this `System` operate on.
+  fn operates_on(&self) -> Vec<TypeId>;
+
+  /// Operate on the `Entity`s using the `Globals`.
+  fn process_entities(&self, entities: Vec<&Entity>, globals: &Globals);
+}
+
+impl <T: EntitySystem> System for T {
+  fn process(&self, world_data: &mut WorldData) {
+    let filtered_entities = world_data.entities.iter()
+          .filter(| e | self.operates_on().into_iter().all(| id | e.components.contains_key(&id)))
+          .collect();
+    self.process_entities(filtered_entities, &world_data.globals);
+  }
 }
