a4a9f94ebebfb0809160c8eda1e70e6030aeef44
David Edmonds <edmonds.d.r@gmail.com>
2017-01-23 08:30:41 +0000

    Add with_label_mut to Entities.

--- src/entities.rs
+++ src/entities.rs
@@ -31,6 +31,13 @@ impl Entities {
             .collect()
     }
 
+    pub fn with_label_mut(&mut self, label: &str) -> Vec<&mut Entity> {
+        self.0
+            .iter_mut()
+            .filter(|e| e.label == label)
+            .collect()
+    }
+
     pub fn with_component<T: 'static>(&self) -> Vec<&Entity> {
         self.0
             .iter()
@@ -109,6 +116,32 @@ mod tests {
         let mut results = entities.with_label("test1");
         assert_eq!(1, results.len());
         assert_eq!("test1", results.pop().unwrap().label);
+        assert!(entities.with_label("aaaaaa").is_empty());
+    }
+
+    #[test]
+    fn with_label_mut_works() {
+        let mut entities = Entities::new();
+        let ent1 = EntityBuilder::create_str("test1")
+            .add(TestComponent(1))
+            .build();
+        let ent2 = EntityBuilder::create_str("test2")
+            .add(AnotherComponent)
+            .build();
+
+        entities.push(ent1);
+        entities.push(ent2);
+
+        {
+            let mut results = entities.with_label_mut("test1");
+            assert_eq!(1, results.len());
+
+            let mut unwrapped = results.pop().unwrap();
+            assert_eq!("test1", unwrapped.label);
+            let mut unwrapped_component = unwrapped.components.get_mut::<TestComponent>().unwrap();
+            unwrapped_component.0 = 2;
+            assert_eq!(unwrapped_component, &mut TestComponent(2));
+        }
         assert!(entities.with_label("aaaaaa").is_empty());
     }
 
